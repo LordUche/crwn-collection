a997da4690e5112dbede66bd733923376f71c2b1
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-04 02:45:05 +0100

    create directory structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Homepage from './Homepage';
+import Homepage from './pages/homepage/homepage.component';
 
-function App() {
+const App = () => {
   return (
     <Homepage />
   );
