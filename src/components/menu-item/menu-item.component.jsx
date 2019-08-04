c2bbfc6892afe8bb16b8c6c3cd64e79d92ddc04c
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-04 03:00:52 +0100

    finish styling menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,8 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => {
 	return (
-		<div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+		<div className={`${size} menu-item`}>
+			<div style={{backgroundImage: `url(${imageUrl})`}} className="background-image"></div>
 			<div className="content">
 				<h1 className="title">{title}</h1>
 				<span className="subtitle">SHOP NOW</span>
