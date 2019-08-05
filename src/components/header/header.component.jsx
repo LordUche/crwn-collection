ef039d895b766a226de750debe369169111631b6
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-05 13:16:41 +0100

    hide or show cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import CartIcon from '../cart-icon/cart-icon.component';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 	<div className="header">
 		<Link className="logo-container" to="/">
 			<Logo className="logo" />
@@ -30,12 +30,13 @@ const Header = ({ currentUser }) => (
 			)}
 			<CartIcon />
 		</div>
-		<CartDropdown />
+		{!hidden && <CartDropdown />}
 	</div>
 );
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden,
 });
 
 export default connect(mapStateToProps)(Header);
