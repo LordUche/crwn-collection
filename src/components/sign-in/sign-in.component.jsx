665a124d0cf83c49a53a9641eb3503b94a8e530b
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-05 01:24:15 +0100

    add sign out feature and style custom button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -42,8 +42,12 @@ class SignIn extends Component {
 						handleChange={this.handleChange}
 						required
 					/>
-					<CustomButton type="submit">Sign In</CustomButton>
-					<CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+					<div className="buttons">
+						<CustomButton type="submit">Sign In</CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							Sign in with Google
+						</CustomButton>
+					</div>
 				</form>
 			</div>
 		);
