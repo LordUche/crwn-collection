c9c4282a8ecf15e21e0833ed580c8507565d5469
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-05 01:02:53 +0100

    listen to oauth state change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import AuthPage from './pages/auth/auth.component';
+import { auth } from './firebase/firebase-utils';
 
-const App = () => {
-	return (
-		<div>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={Homepage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/signin" component={AuthPage} />
-			</Switch>
-		</div>
-	);
-};
+class App extends Component {
+	state = { currentUser: null };
+	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			this.setState({ currentUser: user });
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={Homepage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={AuthPage} />
+				</Switch>
+			</div>
+		);
+	}
+}
 
 export default App;
