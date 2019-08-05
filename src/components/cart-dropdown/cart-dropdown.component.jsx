1e44df4cc762cb2cbea38305b1e0528412fcbff6
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-05 21:26:49 +0100

    render empty-message on cart dropdown if cart is empty

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { selectCartItems } from '../../redux/cart/cart.selectors'
 import './cart-dropdown.styles.scss';
-import { createStructuredSelector } from 'reselect';
 
 const CartDropdown = ({ cartItems }) => {
 	return (
 		<div className="cart-dropdown">
 			<div className="cart-items">
-				{cartItems.map(({ id, ...otherProps }) => <CartItem key={id} {...otherProps} />)}
+				{cartItems.length ? (
+					cartItems.map(({ id, ...otherProps }) => <CartItem key={id} {...otherProps} />)
+				) : (
+					<span className="empty-message">Your cart is empty</span>
+				)}
 			</div>
-			<CustomButton inverted>GO TO CHECKOUT</CustomButton>
+			<CustomButton>GO TO CHECKOUT</CustomButton>
 		</div>
 	);
 };
