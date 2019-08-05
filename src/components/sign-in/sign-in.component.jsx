9912b769bcaad01c31cbecf79387effab1d04912
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-05 08:47:54 +0100

    finish sign in submit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
-import FormInput from '../form-input/form-input.component';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import CustomButton from '../custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
 
 class SignIn extends Component {
 	state = { email: '', password: '' };
 
-	handleSubmit = e => {
+	handleSubmit = async e => {
 		e.preventDefault();
-		this.setState({ email: '', password: '' });
+		const { email, password } = this.state;
+		
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.error(error.message);
+		}
 	};
 
 	handleChange = e => {
