866b45ab26df64f6ad29570dac654bbcfcecf21f
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-06 08:32:08 +0100

    persist redux store

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import './index.css';
+import { persistor, store } from './redux/store';
 import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import {persistStore} from 'redux-persist'
 
 const middleware = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middleware));
-
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const persistor = persistStore(store)
