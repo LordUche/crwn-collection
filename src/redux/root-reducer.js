29a5233658b30102474079c578d17d1672b6f276
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-08-06 08:52:25 +0100

    move directory state into store

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 import userReducer from './user/user.reducer';
 
 const persistConfig = { key: 'root', storage, whitelist: ['cart'] };
@@ -9,6 +10,7 @@ const persistConfig = { key: 'root', storage, whitelist: ['cart'] };
 const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
+	directory: directoryReducer,
 });
 
-export default persistReducer(persistConfig, rootReducer)
+export default persistReducer(persistConfig, rootReducer);
